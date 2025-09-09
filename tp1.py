2f9a6e591f3e7c2467920fdf197db013324f76d6
JDarsaut <jdarsaut@fi.uba.ar>
2025-09-09 16:57:45 -0300

    agrego funcion para minimizar la suma ponderada

--- tp1.py
+++ tp1.py
@@ -1,1 +1,9 @@
-#LLAMADOS DE CODIGOS ACA, EL MAIN 
+def minimizar_suma_ponderada(batallas):
+    prioridad = []
+    for batalla in batallas:
+        ratio = batalla[1] / batalla[0]
+        prioridad.append(batalla)
+
+    batallas_por_prioridad = sorted(prioridad, key=lambda x: x[1]/x[0], reverse=True)
+
+    return batallas_por_prioridad
