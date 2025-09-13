354951d4f63b3a8126b5d987f9a883fbdb980414
Sara Lujan <scasahuaman@fi.uba.ar>
2025-09-13 01:58:02 -0300

    actualizo codigo lee modificación

--- tp1.py
+++ tp1.py
@@ -1,9 +1,3 @@
 def minimizar_suma_ponderada(batallas):
-    prioridad = []
-    for batalla in batallas:
-        ratio = batalla[1] / batalla[0]
-        prioridad.append(batalla)
-
-    batallas_por_prioridad = sorted(prioridad, key=lambda x: x[1]/x[0], reverse=True)
-
-    return batallas_por_prioridad
+    ordenadas_por_prioridad = sorted(batallas, key = lambda x:x[0]/x[1])
+    return ordenadas_por_prioridad
