319efb10bcad2724441237768c1dbdcac08a6de2
Sara Lujan <scasahuaman@fi.uba.ar>
2025-09-13 01:58:32 -0300

    agrego comentario de una linea

--- tp1.py
+++ tp1.py
@@ -1,3 +1,4 @@
+#batallas = (tiempo, importancia)
 def minimizar_suma_ponderada(batallas):
     ordenadas_por_prioridad = sorted(batallas, key = lambda x:x[0]/x[1])
     return ordenadas_por_prioridad
